28dc328f7f2d655a49bafa1c4fca2147e3baa1b2
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2020-10-21 11:13:12 +0545

    Database and Frontend Setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('profile_img')->default('download.jpg');
+            $table->boolean('isAdmin')->default(0);
+            
             $table->rememberToken();
             $table->timestamps();
         });
