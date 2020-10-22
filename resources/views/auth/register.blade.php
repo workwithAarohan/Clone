73061408441ece60d49fbb8f7b11500ee7650138
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2020-10-22 07:51:47 +0545

    Conflict removed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,23 +3,6 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-<<<<<<< HEAD
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-=======
         <div class="col-4 bg-white shadow-sm border p-5 text-center">
                     <h1>Instagram</h1>
                     <h4 class="text-muted my-3">Sign up to see photos and videos from your friends.</h4>
@@ -31,23 +14,10 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror bg-light shadow-none" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
->>>>>>> 330b869868e9dcb63cea4ff9381a6b9a84a1cd8f
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-<<<<<<< HEAD
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-=======
                         </div>
 
 
@@ -55,57 +25,20 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror bg-light shadow-none" placeholder="Full Name" name="name" value="{{ old('name') }}" required autocomplete="name">
 
                                 @error('name')
->>>>>>> 330b869868e9dcb63cea4ff9381a6b9a84a1cd8f
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-<<<<<<< HEAD
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-=======
                         </div>
 
                         <div class="form-group my-1">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror bg-light shadow-none" placeholder="Username" name="username" value="{{ old('username') }}" required autocomplete="username">
 
                                 @error('username')
->>>>>>> 330b869868e9dcb63cea4ff9381a6b9a84a1cd8f
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-<<<<<<< HEAD
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-=======
                         </div>
 
                         <div class="form-group mt-1">
@@ -129,7 +62,6 @@
     <div class="row justify-content-center mt-4">
         <div class="col-4 bg-white shadow-sm border p-4 text-center">
             <p class="mb-0">Have an account? <a href="{{ route('login') }}">{{ __('Login') }}</a></p>
->>>>>>> 330b869868e9dcb63cea4ff9381a6b9a84a1cd8f
         </div>
     </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-<<<<<<< HEAD
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-=======
             <div class="row p-5 bg-white border rounded">
             
             </div>
@@ -32,7 +16,6 @@
         <div class="d-none d-lg-block col-lg-4 p-5">
             Hello
         </div>
->>>>>>> 330b869868e9dcb63cea4ff9381a6b9a84a1cd8f
     </div>
 </div>
 @endsection
